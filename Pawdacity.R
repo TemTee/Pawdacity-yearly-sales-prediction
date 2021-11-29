5464297296c7c72d89dcd449b3f4b4ecc6054bc1
TemTee <temmyteeliz@gmail.com>
2021-11-29 13:03:23 +0100

    1st commit

---
+++ Pawdacity.R
@@ -0,0 +1,13 @@
+#Business and Data Understanding
+#Predict yearly sales of Pawdacity’s Wyoming 13 stores and 
+#recommend the city for the organisation’s proposed 14th store.
+#Data required for analysis are : City’s population, 
+#annual sales by each store, land area, house hold income and total families.
+
+
+library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(ggplot2)
+
