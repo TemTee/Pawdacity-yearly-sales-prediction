87ac80b83aff568ba7b165a01b68777594eda3a7
TemTee <temmyteeliz@gmail.com>
2021-12-09 21:54:47 +0100

    update

--- Pawdacity.R
+++ Pawdacity.R
@@ -11,3 +11,14 @@ library(tidyr)
 library(lubridate)
 library(ggplot2)
 
+paw_mnthly_sales <- read_csv("~/R/datasets/Pawdacity Data Set/Pawdacity monthly Sales.csv")
+paw_total_sales <- read_csv("~/R/datasets/Pawdacity Data Set/Pawdacity total Sales.csv")
+pop_data <- read_csv("~/R/datasets/Pawdacity Data Set/Population Data.csv")
+demograph_data <- read_csv("~/R/datasets/Pawdacity Data Set/Demographic Data.csv")
+competitor_sales <- read_csv("~/R/datasets/Pawdacity Data Set/Competitors Sales.txt")
+
+
+head(paw_mnthly_sales)
+head(paw_total_sales)
+
+
